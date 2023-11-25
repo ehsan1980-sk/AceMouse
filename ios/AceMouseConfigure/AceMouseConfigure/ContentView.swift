f9b4574740f49ca07d6a24b8cedac4effd83fc32
will wade <willwade@gmail.com>
2023-11-25 18:14:02 +0000

    updating swiftcode.

--- ios/AceMouseConfigure/AceMouseConfigure/ContentView.swift
+++ ios/AceMouseConfigure/AceMouseConfigure/ContentView.swift
@@ -4,6 +4,7 @@ import CoreBluetooth
 class BluetoothManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeripheralDelegate {
     var centralManager: CBCentralManager!
     @Published var isConnected = false
+    @Published var calibrationInstructions = ""  // Moved here
     var aceMousePeripheral: CBPeripheral?  // Declare the peripheral property
 
     override init() {
@@ -25,7 +26,7 @@ class BluetoothManager: NSObject, ObservableObject, CBCentralManagerDelegate, CB
 
                 // Save a reference to the peripheral
                 self.aceMousePeripheral = peripheral  // Save the reference
-                self.aceMousePeripheral?.delegate = self  
+                self.aceMousePeripheral?.delegate = self
 
                 // Connect to the peripheral
                 central.connect(peripheral, options: nil)
@@ -35,48 +36,101 @@ class BluetoothManager: NSObject, ObservableObject, CBCentralManagerDelegate, CB
 
 
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        // Connection successful, discover services
-    }
+        // Connection successful
+        print("Connected to \(peripheral.name ?? "")")
 
-    // Add other delegate methods to handle discovery, data transfer, etc.
+        // Discover services here. You can pass nil to discover all services or pass an array of CBUUIDs if you're looking for specific services
+        peripheral.discoverServices(nil)  // or peripheral.discoverServices([CBUUID(string: "YOUR_UART_SERVICE_UUID")])
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let error = error {
+            print("Error receiving notification from characteristic: \(error.localizedDescription)")
+            return
+        }
+
+        if let value = characteristic.value,
+           let instruction = String(data: value, encoding: .utf8) {
+            // Update the UI with the new instruction
+            DispatchQueue.main.async {
+                self.calibrationInstructions = instruction
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if let error = error {
+            print("Error discovering characteristics: \(error.localizedDescription)")
+            return
+        }
+
+        guard let characteristics = service.characteristics else { return }
+
+        for characteristic in characteristics {
+            if characteristic.uuid == CBUUID(string: "INSTRUCTION_CHARACTERISTIC_UUID") {
+                peripheral.setNotifyValue(true, for: characteristic)
+            }
+        }
+    }
+    
+    func findCommandCharacteristic() -> CBCharacteristic? {
+           guard let services = aceMousePeripheral?.services else {
+               return nil
+           }
+
+           for service in services {
+               for characteristic in service.characteristics ?? [] {
+                   if characteristic.uuid == CBUUID(string: "COMMAND_CHARACTERISTIC_UUID") {
+                       return characteristic
+                   }
+               }
+           }
+
+           return nil
+       }
+
+
 }
 
+
+
 struct ContentView: View {
-    @State private var isConnected = false
-    @State private var calibrationInstructions = ""
     @ObservedObject var bluetoothManager = BluetoothManager()
     
     var body: some View {
         NavigationView {
             VStack {
-                if isConnected {
+                if bluetoothManager.isConnected {
                     Text("Connected to AceMouse")
                         .foregroundColor(.green)
                     
-                    if !calibrationInstructions.isEmpty {
-                        Text("Instruction: \(calibrationInstructions)")
+                    if !bluetoothManager.calibrationInstructions.isEmpty {
+                        Text("Instruction: \(bluetoothManager.calibrationInstructions)")
                     }
 
                     Button("Calibrate") {
-                        // Trigger calibration process
                         startCalibration()
                     }
                 } else {
                     Text("Searching for AceMouse...")
                         .foregroundColor(.orange)
-                    // Add logic to search for AceMouse
                 }
             }
             .navigationBarTitle("AceMouse Calibration")
         }
-        .onAppear {
-            // Add logic to start scanning for devices
-        }
     }
 
     private func startCalibration() {
-        // Add logic to send calibration command
-        // Update `calibrationInstructions` based on responses
+        guard let peripheral = bluetoothManager.aceMousePeripheral,
+              let commandCharacteristic = bluetoothManager.findCommandCharacteristic() else {
+            print("Peripheral or characteristic not found")
+            return
+        }
+
+        let calibrationCommand = "Start Calibration"
+        if let data = calibrationCommand.data(using: .utf8) {
+            peripheral.writeValue(data, for: commandCharacteristic, type: .withResponse)
+        }
     }
 }
 
